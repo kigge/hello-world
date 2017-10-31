fdef6c1e61de7cffc08a9b90f60b5c8c47855026
Christoffer Lander <christoffer.lander@noresca.se>
2017-10-31 22:28:32 +0100

    updated demo.js + merge

---
+++ demo.js
@@ -0,0 +1,17 @@
+/**
+* @ApiVersion 2.x
+* @ScriptType UserEventScript
+*/
+
+define(["N/record"], function(r) {
+	exports = {};
+
+	function beforeSubmit(context){
+		
+	}
+
+	exports.beforeSubmit = beforeSubmit;
+	return {
+		berforeSubmit : beforeSubmit
+	};
+});
